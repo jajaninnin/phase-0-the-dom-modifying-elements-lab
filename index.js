b44dbf0b35f3f7d4709b9cea071eb1103285d703
janine pamintuan <jaja.ninnin@gmail.com>
2024-09-23 11:08:18 -0400

    dom mod

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+// Write your code here
+const main = document.getElementById('main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = '<h1>Janine is the champion</h1>';
